6892a83be31482c2d432a9b9990da158dda069b1
padho <p.holl@outlook.com>
2022-06-14 13:59:10 +0200

    Add pandas and numpy to dev requirements.txt

---
+++ examples/service_definition.py
@@ -0,0 +1,16 @@
+# Import Fusionbase
+import sys
+sys.path.append("/Users/patrick/Development/fusionbase/v2/public/fusionbase-python/fusionbase/")
+sys.path.append("/Users/patrick/Development/fusionbase/v2/public/fusionbase-python/")
+from fusionbase.Fusionbase import Fusionbase
+import os
+
+# Create a new datastream
+# Provide your API Key and the Fusionbase API URI (usually: https://api.fusionbase.com/api/v1)
+fusionbase = Fusionbase(auth={"api_key": os.getenv('FUSIONBASE_API_KEY')},
+                      connection={"base_uri": "https://api.fusionbase.com/api/v1"})
+
+
+data_service_key = "23622632"
+data_service = fusionbase.get_dataservice(key=data_service_key)
+data_service.pretty_meta_data()
